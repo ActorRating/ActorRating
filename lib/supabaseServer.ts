b17459c1af3d9ec1aecaf369fd34e879c077f35e
Demir Horzum <demirhorzum@Demirs-Mac-mini.local>
2025-09-25 19:18:27 +0300

    fix: add TypeScript types to supabaseServer.ts
    - add NextApiRequest and NextApiResponse types to fix build error
    - ESLint dependency added

--- lib/supabaseServer.ts
+++ lib/supabaseServer.ts
@@ -1,6 +1,7 @@
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export function createServerSupabase(req, res) {
+export function createServerSupabase(req: NextApiRequest, res: NextApiResponse) {
   return createServerSupabaseClient({ req, res })
 }
 
